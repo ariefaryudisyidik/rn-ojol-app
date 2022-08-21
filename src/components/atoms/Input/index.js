bac2565b247a0be4e252614b9579f1f88349af8a
ariefaryudisyidik <ariefaryudisyidik@gmail.com>
2022-08-21 21:49:54 +0700

    Update Register page to dynamic and hide password

--- src/components/atoms/Input/index.js
+++ src/components/atoms/Input/index.js
@@ -2,13 +2,14 @@ import React from 'react';
 import {StyleSheet, TextInput, View} from 'react-native';
 import {colors} from '../../../utils';
 
-const Input = ({placeholder}) => {
+const Input = ({placeholder, ...res}) => {
   return (
     <View>
       <TextInput
         style={styles.input}
         placeholder={placeholder}
         placeholderTextColor={colors.default}
+        {...res}
       />
     </View>
   );
--- src/pages/Register/index.js
+++ src/pages/Register/index.js
@@ -1,27 +1,58 @@
-import React from 'react';
-import {Text, View, StyleSheet, Image} from 'react-native';
-import {Input} from '../../components/atoms';
-import {colors} from '../../utils';
+import React, {useState} from 'react';
+import {Image, ScrollView, StyleSheet, Text, View} from 'react-native';
 import {IconBack, IllustrationRegister} from '../../assets';
+import {Button, Input} from '../../components/atoms';
+import {colors} from '../../utils';
 
 const Register = () => {
+  const [form, setForm] = useState({
+    fullName: '',
+    email: '',
+    password: '',
+  });
+
+  const sendData = () => {
+    console.log('Data yang dikirim: ', form);
+  };
+
+  const onInputChange = (value, input) => {
+    setForm({...form, [input]: value});
+  };
+
   return (
     <View style={styles.wrapper.page}>
-      <Image source={IconBack} style={styles.iconBack} />
-      <IllustrationRegister
-        width={148}
-        height={92}
-        style={styles.illustration}
-      />
-      <Text style={styles.text.desc}>
-        Mohon mengisi beberapa data untuk proses daftar anda
-      </Text>
-      <View style={styles.space(64)} />
-      <Input placeholder="nama lengkap" />
-      <View style={styles.space(32)} />
-      <Input placeholder="email" />
-      <View style={styles.space(32)} />
-      <Input placeholder="password" />
+      <ScrollView showsVerticalScrollIndicator={false}>
+        <Image source={IconBack} style={styles.iconBack} />
+        <IllustrationRegister
+          width={148}
+          height={92}
+          style={styles.illustration}
+        />
+        <Text style={styles.text.desc}>
+          Mohon mengisi beberapa data untuk proses daftar anda
+        </Text>
+        <View style={styles.space(64)} />
+        <Input
+          placeholder="nama lengkap"
+          value={form.fullName}
+          onChangeText={value => onInputChange(value, 'fullName')}
+        />
+        <View style={styles.space(32)} />
+        <Input
+          placeholder="email"
+          value={form.email}
+          onChangeText={value => onInputChange(value, 'email')}
+        />
+        <View style={styles.space(32)} />
+        <Input
+          placeholder="password"
+          value={form.password}
+          onChangeText={value => onInputChange(value, 'password')}
+          secureTextEntry
+        />
+        <View style={styles.space(80)} />
+        <Button title="Daftar" onPress={sendData} />
+      </ScrollView>
     </View>
   );
 };
