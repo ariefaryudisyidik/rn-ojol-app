049a1e488a84ed2677005142ed5b8d9c03ed451b
ariefaryudisyidik <ariefaryudisyidik@gmail.com>
2022-08-21 17:02:44 +0700

    Add image and custom component

---
+++ src/components/atoms/Button/index.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import {Text, TouchableOpacity} from 'react-native';
+import {colors} from '../../../utils';
+
+const Button = ({title}) => {
+  return (
+    <TouchableOpacity style={styles.wrapper.component}>
+      <Text style={styles.text.title}>{title}</Text>
+    </TouchableOpacity>
+  );
+};
+
+const styles = {
+  wrapper: {
+    component: {backgroundColor: colors.default, borderRadius: 25},
+  },
+  text: {
+    title: {
+      fontSize: 12,
+      fontWeight: 'bold',
+      color: 'white',
+      textTransform: 'uppercase',
+      textAlign: 'center',
+      paddingVertical: 13,
+    },
+  },
+};
+
+export default Button;
--- src/pages/WelcomeAuth/index.js
+++ src/pages/WelcomeAuth/index.js
@@ -1,72 +1,48 @@
 import React from 'react';
-import {Text, TouchableOpacity, View} from 'react-native';
-
-const AcitionButton = ({desc, title}) => {
-  return (
-    <View style={{marginBottom: 43, maxWidth: 225}}>
-      <Text
-        style={{
-          fontSize: 10,
-          color: '#7E7E7E',
-          textAlign: 'center',
-          paddingHorizontal: '15%',
-          marginBottom: 6,
-        }}>
-        {desc}
-      </Text>
-      <TouchableOpacity style={{backgroundColor: '#A55EEA', borderRadius: 25}}>
-        <Text
-          style={{
-            fontSize: 12,
-            fontWeight: 'bold',
-            color: 'white',
-            textTransform: 'uppercase',
-            textAlign: 'center',
-            paddingVertical: 13,
-          }}>
-          {title}
-        </Text>
-      </TouchableOpacity>
-    </View>
-  );
-};
+import {Image, Text, View} from 'react-native';
+import {welcomeAuth} from '../../assets';
+import {colors} from '../../utils';
+import ActionButton from './ActionButton';
 
 const WelcomeAuth = () => {
   return (
-    <View
-      style={{
-        alignItems: 'center',
-        justifyContent: 'center',
-        flex: 1,
-        backgroundColor: 'white',
-      }}>
-      <View
-        style={{
-          width: 219,
-          height: 117,
-          backgroundColor: '#A55EEA',
-          marginBottom: 10,
-        }}
-      />
-      <Text
-        style={{
-          fontSize: 18,
-          fontWeight: 'bold',
-          color: '#A55EEA',
-          marginBottom: 76,
-        }}>
-        Selamat datang di O-JOL
-      </Text>
-      <AcitionButton
+    <View style={styles.wrapper.page}>
+      <Image source={welcomeAuth} style={styles.wrapper.illustration} />
+      <Text style={styles.text.welcome}>Selamat datang di O-JOL</Text>
+      <ActionButton
         desc=" Silahkan masuk, jika anda sudah memiliki akun"
         title="Masuk"
       />
-      <AcitionButton
+      <ActionButton
         desc="atau Silahkan daftar jika anda belum memiliki akun"
         title="Daftar"
       />
     </View>
   );
+};
+
+const styles = {
+  wrapper: {
+    page: {
+      alignItems: 'center',
+      justifyContent: 'center',
+      flex: 1,
+      backgroundColor: 'white',
+    },
+    illustration: {
+      width: 219,
+      height: 117,
+      marginBottom: 10,
+    },
+  },
+  text: {
+    welcome: {
+      fontSize: 18,
+      fontWeight: 'bold',
+      color: colors.default,
+      marginBottom: 76,
+    },
+  },
 };
 
 export default WelcomeAuth;
